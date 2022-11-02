47b3d9154bbae2cc76ca3605af57880fa909ba76
Iolanta Averyanova <iolantazhiznevskaya@gmail.com>
2022-11-02 18:20:51 +0300

    Fix styles

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,8 +4,10 @@ import './Main.css';
 
 const Main = () => {
   return (
-    <> <h1 className='mt-5'>My User List</h1>
-    <div className='bg'></div></>
+    <> 
+    <h1 className='mt-5'>My User List</h1>
+    <div className='bg'></div>
+    </>
    
   )
 }
